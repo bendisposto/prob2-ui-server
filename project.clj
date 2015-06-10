d0edabc3869dc851bdced3ea433c25fc93aefbcc
Jens Bendisposto <jens@bendisposto.de>
2015-06-10 09:49:24 +0200

    deployment

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.prob2 "0.1.0-SNAPSHOT"
+(defproject de.prob2.ui-server "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
